a0a8688d70de2ec450a46412309d84a0cf3393ec
Samu <n2kasa01@students.oamk.fi>
2024-03-20 10:26:23 +0200

    Test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Text 2133</Text>
+      <Text>Text 21333</Text>
       <StatusBar style="auto" />
     </View>
   );
