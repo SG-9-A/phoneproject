23b6fd1d5836b9311d58bed337fc122d113aba28
Samu <n2kasa01@students.oamk.fi>
2024-03-20 11:00:11 +0200

    Sprite update

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Sprite from "./src/components/object-graphics/Sprite";
+import { useEffect, useState } from "react";
+import { SPRITE_SHEET_SRC } from "./src/components/object-graphics/Sprite";
 
-export default function App() {
+function App() {
+  const [spriteSheetImage, setSpriteSheetImage] = useState(null);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = SPRITE_SHEET_SRC;
+    image.onload = () => {
+      setSpriteSheetImage(image);
+    };
+  }, []);
+
+  if (!spriteSheetImage) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Text 21333</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div>
+      <Sprite image={spriteSheetImage} frameCoord={"1x0"} />
+      <Sprite image={spriteSheetImage} frameCoord={"0x2"} />
+      <Sprite image={spriteSheetImage} frameCoord={"0x3"} />
+    </div>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
